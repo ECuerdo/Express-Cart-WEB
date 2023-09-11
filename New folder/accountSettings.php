572a3d6498d3aa2c349d3ee44aa2040ae7c9f4db
EuLo C <eulysiscuerdo03@gmail.com>
2023-09-12 05:14:53 +0800

    Updated Dashboard

---
+++ New folder/accountSettings.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express Cart</title>
+    <link rel="stylesheet" href="./assets/css/Clientstyle.css">
+</head>
+
+<body>
+
+    <ul class="navbar">
+        <li><a href="#news">gluacan</a></li>
+        <li><a href="#home">Notification</a></li>
+    </ul>
+    <div class="navbar-bottom">
+        <div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
