6298b9d044efe37588b078b1985881a31c2f7dd9
EuLo C <eulysiscuerdo03@gmail.com>
2023-11-05 07:58:26 +0800

    Add to cart Functionality

--- ExpressCart/public/items.js
+++ ExpressCart/public/items.js
@@ -2,13 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const itemForm = document.getElementById("post-item-form");
     const itemList = document.getElementById("items");
 
+    // Event listener for submitting the item form
     itemForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
+        // Get item details from the form
         const itemName = document.getElementById("item-name").value;
         const itemPrice = document.getElementById("item-price").value;
         const itemImage = document.getElementById("item-image").files[0];
 
+        // Create form data to send via POST request
         const formData = new FormData();
         formData.append("itemName", itemName);
         formData.append("itemPrice", itemPrice);
@@ -27,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const itemHTML = `
                     <strong>${itemName}</strong> - $${itemPrice}
                     <img src="${data.imagePath}" alt="${itemName}" style="max-width: 100px; max-height: 100px;">
+                    <button class="add-to-cart-btn" data-id="${data.itemId}" data-name="${itemName}" data-price="${itemPrice}" data-image="${data.imagePath}">Add to Cart</button>
                 `;
                 listItem.innerHTML = itemHTML;
                 itemList.appendChild(listItem);
@@ -35,6 +39,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById("item-name").value = "";
                 document.getElementById("item-price").value = "";
                 document.getElementById("item-image").value = "";
+
+                // Add event listener for Add to Cart button
+                const addToCartButton = listItem.querySelector(".add-to-cart-btn");
+                addToCartButton.addEventListener("click", addToCart);
             } else {
                 alert("Error posting item: " + data.error);
             }
@@ -43,6 +51,34 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Error:", error);
         });
     });
+
+    // Function to handle adding items to the cart
+    function addToCart(event) {
+        const itemId = event.target.getAttribute("data-id");
+        const itemName = event.target.getAttribute("data-name");
+        const itemPrice = parseFloat(event.target.getAttribute("data-price"));
+        const itemImage = event.target.getAttribute("data-image");
+
+        // Retrieve cart data from cookies
+        let listCart = [];
+        const cookieValue = document.cookie.split('; ').find(row => row.startsWith('listCart='));
+        if (cookieValue) {
+            listCart = JSON.parse(cookieValue.split('=')[1]);
+        }
+
+        // Add the selected item to the cart
+        const cartItem = { id: itemId, name: itemName, price: itemPrice, image: itemImage, quantity: 1 };
+        listCart.push(cartItem);
+
+        // Save the updated cart in the cookie
+        document.cookie = `listCart=${JSON.stringify(listCart)};path=/`;
+
+        // Perform any other actions you need with the updated cart
+        // ...
+
+        // Example: Update cart display
+        // addCartToHTML();
+    }
 
     // Fetch items from the server when the page loads
     fetch("/index")
@@ -54,9 +90,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 const itemHTML = `
                     <strong>${item.name}</strong> - $${item.price}
                     <img src="${item.image}" alt="${item.name}" style="max-width: 100px; max-height: 100px;">
+                    <button class="add-to-cart-btn" data-id="${item.id}" data-name="${item.name}" data-price="${item.price}" data-image="${item.image}">Add to Cart</button>
                 `;
                 listItem.innerHTML = itemHTML;
                 itemList.appendChild(listItem);
+
+                // Add event listener for Add to Cart button
+                const addToCartButton = listItem.querySelector(".add-to-cart-btn");
+                addToCartButton.addEventListener("click", addToCart);
             });
         })
         .catch(error => {
